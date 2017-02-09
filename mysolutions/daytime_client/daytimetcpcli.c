804b3f4d01a9997678b3f639db0831aff7f46bfe
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-09 21:03:48 -0200

    Daytime client, first version

---
+++ mysolutions/daytime_client/daytimetcpcli.c
@@ -0,0 +1,50 @@
+#include <stdio.h>      /* printf, perror */
+#include <stdlib.h>     /* exit */
+#include <unistd.h>     /* read, write, close */
+#include <string.h>     /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#define MAXLINE 4096	/* max text line length */
+#include <netdb.h>      /* struct hostent, gethostbyname */
+
+int main(int argc, char **argv)
+{
+	int	sockfd, n;
+	char	recvline[MAXLINE + 1];
+	struct  sockaddr_in servaddr;
+	struct  hostent *server;
+
+	if (argc != 2) {
+		perror("usage: a.out <IPaddress>");
+		exit(1);
+	}
+
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		perror("socket error");
+
+	if ( (server = gethostbyname(argv[1])) == NULL ) {
+	  perror("Error, no such host!");
+		exit(1);
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port   = htons(13);	/* daytime server */
+
+	memcpy(&servaddr.sin_addr.s_addr, server -> h_addr, server -> h_length);
+
+	if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+		perror("connect error");
+
+	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+		recvline[n] = 0;	/* null terminate */
+		if (fputs(recvline, stdout) == EOF)
+			perror("fputs error");
+	}
+
+	if (n < 0)
+		perror("read error");
+
+	close(sockfd);
+	return(0);
+}
