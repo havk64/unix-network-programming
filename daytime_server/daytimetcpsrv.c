1d22ec6f5aa7166bf77e1384f17b78c7c4c577f0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-13 00:17:29 -0200

    Add comments

--- daytime_server/daytimetcpsrv.c
+++ daytime_server/daytimetcpsrv.c
@@ -1,3 +1,11 @@
+/* Simple Daytime server
+ * Compile:
+ *  $ gcc -Wall -Werror -Wextra -pedantic daytimetcpsrv.c -o daytimetcpsrv
+ *
+ * Usage:
+ *  $ sudo ./daytimesrv (Bind might need root privileges)
+ */
+
 #include <stdio.h>      /* printf, perror */
 #include <stdlib.h>     /* exit */
 #include <unistd.h>     /* read, write, close */
@@ -15,27 +23,28 @@ int main()
 	struct	sockaddr_in servaddr;
 	char	buff[MAXLINE];
 	time_t	ticks;
-
+	/* Create the socket... */
 	if ( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("Socket error");
 		exit(1);
 	}
-
+	/* Fill Internet socket address */
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(13);	/* daytime server */
-
+	/* Bind server port to socket... */
 	if( (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) ) < 0) {
 		perror("bind error");
 		exit(1);
 	}
-
+	/* Convert socket to listening socket */
 	if( (listen(listenfd, LISTENQ)) < 0) {
 		perror("listen error");
 		exit(1);
 	}
-
+	printf("Listening on localhost...\n");
+	/* Accept client connection, send reply... */
 	for ( ; ; ) {
 		if( (connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) < 0) {
 			perror("accept error");
